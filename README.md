237f3925a04cea3e5c9966ca15e6aea7c6fe1451
Emerson Silva <emerson_jdss@hotmail.com>
2024-07-08 13:09:43 -0300

    test site

---
+++ README.md
@@ -0,0 +1,3 @@
+# QRCode Test PortoNet
+
+This site aims to carry out an unscheduled test for Schema url on Android and IOS
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import qrCode from "./assets/qrcode.png";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 w-full h-full p-4 flex items-center justify-center">
+        <div className="h-1/2 w-1/2 flex items-center justify-center p-3">
+          <figure>
+            <img src={qrCode} alt="Logo" />
+          </figure>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
